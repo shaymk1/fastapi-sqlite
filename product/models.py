74f2f5e73c8d57d6b27c19c7fb4bf90ac4c4d3cc
shaymk1 <mkekae@gmail.com>
2021-11-16 14:12:52 +0200

    performed CRUD operations on the database

---
+++ product/models.py
@@ -0,0 +1,20 @@
+
+
+from sqlalchemy import Column, Integer, String
+from .database import Base
+
+
+class Product(Base):
+    __tablename__ = 'products'
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    description = Column(String)
+    price = Column(Integer)
+
+
+class Seller(Base):
+    __tablename__ = 'sellers'
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String)
+    email = Column(String)
+    password = Column(String)
--- product/schemas.py
+++ product/schemas.py
@@ -7,4 +7,19 @@ class Product(BaseModel):
     description : str
     price: int
 
+
+#if you want some fields not to display
+class DisplayProduct(BaseModel):
+    title: str
+    description: str
+    class Config:
+        orm_mode = True
+
+class Seller(BaseModel):
+    username: str
+    email:str
+    password:str
+
     
+    
+        
